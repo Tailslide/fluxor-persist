fce78f29ec17f9452928b11db29029f2747c2a47
Greg Pringle <5757188+Tailslide@users.noreply.github.com>
2021-03-19 15:02:27 -0600

    Code tidy

--- Fluxor.Persist.Sample/Storage/LocalStateStorage.cs
+++ Fluxor.Persist.Sample/Storage/LocalStateStorage.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Blazored.LocalStorage;
+﻿using Blazored.LocalStorage;
 using Fluxor.Persist.Storage;
+using System.Threading.Tasks;
 
 namespace Fluxor.Persist.Sample.Storage
 {
